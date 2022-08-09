67fdcb7ac70af9202a8774e5ca96613b76ed4ed0
Костаревы <@->
2022-08-09 17:26:52 +0500

    Добавьте файлы проекта.

---
+++ lab1.2/lab1.2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+	float n, m, a, b, c;
+	cin >> n;
+	cin >> m;
+	a = m++ - -n;
+	cout << a << endl;
+
+	cin >> n;
+	cin >> m;
+	b = m++ > ++n;
+	cout << b << endl;
+
+	cin >> n;
+	cin >> m;
+	c = n-- > --m;
+	cout << c << endl;
+
+}
